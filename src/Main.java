d640db66bc45fdfc8070b6767c9d9e413eec58e7
exteriorduke319 <yarnevictanner@gmail.com>
2023-12-19 20:15:49 -0500

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,75 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner kanye = new Scanner(System.in);
+
+        Player player1 = new Player();
+        String playerNameInput;
+        int playerXInput;
+        int playerYInput;
+        int playerZInput;
+        int playerHpInput;
+        int playerDirectionInput;
+
+        // Player 2 setup
+        playerNameInput = InputHelper.getNonZeroLenString(kanye, "Player 2, what is your name?");
+
+        playerXInput = InputHelper.getInt(kanye, "What is your x-coordinate?");
+
+        playerYInput = InputHelper.getInt(kanye, "What is your y-coordinate?");
+
+        playerZInput = InputHelper.getInt(kanye, "What is your z-coordinate?");
+
+        Player player2 = new Player(playerNameInput, playerXInput, playerYInput, playerZInput);
+
+        // Player 3 setup
+        playerNameInput = InputHelper.getNonZeroLenString(kanye, "Player 3, what is your name?");
+
+        playerXInput = InputHelper.getInt(kanye, "What is your x-coordinate?");
+
+        playerYInput = InputHelper.getInt(kanye, "What is your y-coordinate?");
+
+        playerZInput = InputHelper.getInt(kanye, "What is your z-coordinate?");
+
+        playerHpInput = InputHelper.getPositiveNonZeroInt(kanye, "What is your hp?");
+
+        playerDirectionInput = InputHelper.getRangedInt(kanye, "What is your direction?  North = 1, South = 2, Up = 3, Down = 4, East = 5, West = 6", 1, 6);
+
+        Player player3 = new Player(playerNameInput, playerXInput, playerYInput, playerZInput, playerHpInput, playerDirectionInput);
+
+
+        // Gameplay
+
+        System.out.println(player1.toString());
+        System.out.println();
+
+        System.out.println(player2.toString());
+        System.out.println();
+
+        System.out.println(player3.toString());
+        System.out.println();
+
+        // Move
+        System.out.println(player3.getName() + " current Y: " + player3.getY());
+        player3.move(3, 7);
+        System.out.println(player3.getName() + " new Y: " + player3.getY());
+        System.out.println();
+
+
+        // Teleport
+        player1.teleport(player2);
+        System.out.println(player1.getName() + " teleports to " + player2.getName());
+        System.out.println(player1.getName() + "Coordinates: X " + player1.getX() + " Y " + player1.getY() + " Z " + player1.getZ());
+        System.out.println();
+
+        // Attack
+        player2.attack(player1, 10);
+        System.out.println(player2.getName() + " attacks " + player1.getName() + "! 10 damage dealt, " + player1.getName() + "'s hp is now " + player1.getHp() + " and " + player2.getName() + "'s hp is now " + player2.getHp());
+        System.out.println();
+
+        // Get Distance
+        System.out.println(player3.getName() + " is " + player3.getDistance(player2) + " units away from " + player2.getName());
+        System.out.println(player3.getName() + " is " + player3.getDistance(player2.getX(), player2.getY(), player2.getZ()) + " units away from " + player2.getName());
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,140 @@
-package PACKAGE_NAME;public class Player {
+public class Player {
+    // Class attributes(variables/fields)
+    private String name;
+    private int numPlayers = 1;
+    private int x;
+    private int y;
+    private int z;
+    private int direction; // North = 1, South = 2, Up = 3, Down = 4, East = 5, West = 6
+    private int hp;
+
+
+    // Constructors - build objects
+    public Player () {
+        name = "P" + numPlayers;
+        x = 0;
+        y = 0;
+        z = 0;
+        direction = 1;
+        hp = 20;
+    }
+
+    public Player(String name, int x, int y, int z){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        direction = 1;
+        hp = 20;
+    }
+
+    public Player(String name, int x, int y, int z, int hp, int direction){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.direction = direction;
+        this.hp = hp;
+    }
+
+    // Accessor methods
+
+    public int getNumPlayers() {
+        return numPlayers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public int getZ(){
+        return z;
+    }
+
+    public int getHp(){
+        return hp;
+    }
+
+    public int getDirection(){
+        return direction;
+    }
+
+    // Mutator methods(setters)
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public void move(int direction, int units) {
+        if (direction > 0 || direction <= 6) {
+            this.direction = direction;
+            if (direction == 1) {
+                this.x += units;
+            } else if (direction == 2) {
+                this.x -= units;
+            } else if (direction == 3) {
+                this.y += units;
+            } else if (direction == 4) {
+                this.y -= units;
+            } else if (direction == 5) {
+                this.z += units;
+            } else if (direction == 6) {
+                this.z -= units;
+            }
+        } else {
+            System.out.println("Error: direction value must be between 1 and 6");
+        }
+    }
+
+    public void teleport(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void teleport(Player player) {
+        x = player.x;
+        y = player.y;
+        z = player.z;
+    }
+
+    public void attack(Player player, int damage) {
+        player.hp -= damage;
+
+        if(player.hp < 0) {
+            player.hp = 0;
+        }
+
+        hp += (damage*0.5);
+    }
+
+    public String toString(){
+        String s = ("Name: " + name + "\nHealth: " + hp + "\nCoordinates: X " + x + " Y " + y + " Z " + z + "\nDirection: " + direction);
+        return s;
+    }
+
+    // Other
+
+    public double getDistance(int x, int y, int z) {
+        double distance = Math.sqrt(Math.pow((x - this.x), 2) + Math.pow((y - this.y), 2) + Math.pow((z - this.z), 2));
+        return distance;
+    }
+
+    public double getDistance(Player player) {
+        double distance = Math.sqrt(Math.pow((player.x - this.x), 2) + Math.pow((player.y - this.y), 2) + Math.pow((player.z - this.z), 2));
+        return distance;
+    }
 }
+
